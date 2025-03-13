18aed1fecfa4d43684457e74f33079a2ea6bebc7
tingying-he <tingying-he@users.noreply.github.com>
2025-03-13 23:09:09 +0000

    deploy: 0b2bff065c8b428f4a88c805e3ae611199575515

--- pattern-design-study/assets/tools/init-index.js
+++
@@ -1,8 +0,0 @@
-import {checkWindowDimension} from './sizecheck.js';
-
-$(window).on('load', function () {
-  checkWindowDimension();
-  $('#loader').delay(100).fadeOut('slow');
-  // Hide the content and show a message asking for resizing the window or using a different device
-  $(window).on('resize', checkWindowDimension);
-});
